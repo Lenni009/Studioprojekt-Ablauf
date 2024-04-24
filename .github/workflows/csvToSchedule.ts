2fd2d491bf22d1e02ee4ba8191e9d7b5781ec27a
Lenni009 <guentherhanslphdyt@gmail.com>
2024-04-24 11:22:26 +0200

    add automated schedule update

---
+++ .github/workflows/csvToSchedule.ts
@@ -0,0 +1,29 @@
+import { RawScheduleItem } from '../../src/types/schedule.ts';
+
+const downloadLocation = './download/';
+const separator = ',';
+const csvPath = `${downloadLocation}Sendeablauf.csv`;
+
+const csvContent = Deno.readTextFileSync(csvPath);
+
+const csvRows = csvContent.replaceAll('\r', '').split('\n');
+
+const csvDataRows = csvRows.slice(1);
+const csvRowData = csvDataRows.map((item) => item.split(separator));
+
+const lastDataIndex = csvRowData.findLastIndex((item) => item[1]);
+const scheduleDataArray: string[][] = csvRowData.slice(0, lastDataIndex + 1);
+
+const scheduleDataObjects: RawScheduleItem[] = [];
+
+for (const item of scheduleDataArray) {
+  const obj: RawScheduleItem = {
+    name: item[1],
+    length: item[0].replaceAll(',0', '') || '0:20',
+  };
+  scheduleDataObjects.push(obj);
+}
+
+const scheduleString = JSON.stringify(scheduleDataObjects, null, 2);
+
+Deno.writeTextFileSync(`${downloadLocation}ablauf.json`, scheduleString);
